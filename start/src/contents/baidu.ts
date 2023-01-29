b0d7c202d4972f032c7b625a5e0470ebb868b368
tuntun <760523867@qq.com>
2023-01-29 16:27:15 +0800

    update start example

--- start/src/contents/baidu.ts
+++ start/src/contents/baidu.ts
@@ -1,8 +1,8 @@
 import type { PlasmoContentScript } from "plasmo"
  
 export const config: PlasmoContentScript = {
-  matches: ['*://www.baidu.com/*'],
-  all_frames: true
+  matches: ['*://*.baidu.com/*'],
+  all_frames: true,
 }
 
 console.log('this is baidu page');
