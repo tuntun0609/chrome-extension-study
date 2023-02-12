b5e615b62a6872c165e6f5d9250367b2eba76eef
tuntun <760523867@qq.com>
2023-02-12 22:25:59 +0800

    inject main example

--- start/src/background.ts
+++ start/src/background.ts
@@ -1,3 +1,18 @@
+import url from 'url:~content/baidu'
+
 export {};
 
-console.log('this is background powered by plamso');
+console.log('this is background powered by plamso', url.split("/").pop().split("?")[0]);
+
+chrome.scripting.registerContentScripts([
+	{
+			"id": "csInMainWorld",
+			"js": [url.split("/").pop().split("?")[0]],
+			"matches": [
+				"*://*.google.com/*"
+			],
+			"allFrames": true,
+			"world": "MAIN",
+			"runAt": "document_start"
+	}
+]);
--- start/src/contents/baidu.ts
+++ start/src/contents/baidu.ts
@@ -1,8 +1,16 @@
-import type { PlasmoContentScript } from "plasmo"
+import type { PlasmoCSConfig } from "plasmo"
  
-export const config: PlasmoContentScript = {
+export const config: PlasmoCSConfig = {
   matches: ['*://*.baidu.com/*'],
   all_frames: true,
+  world: 'MAIN',
+  run_at: 'document_start'
+}
+
+const myJSONStringify = JSON.stringify;
+JSON.stringify = (obj: any) => {
+	console.log('you are using JSON.stringify');
+	return myJSONStringify(obj);
 }
 
 console.log('this is baidu page');
